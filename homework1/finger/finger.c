21b659e44f730458b7d9f09d6e4a873a48f11a2a
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2024-05-05 16:22:06 +0200

    manage users as arguments in finger command

--- homework1/finger/finger.c
+++ homework1/finger/finger.c
@@ -13,25 +13,42 @@ int main(int argc, char* argv[]) {
     return EXIT_FAILURE;
   }
 
-  int result = parseArguments(argc, argv, options);
+  int result = parseOptions(argc, argv, options);
   if (result == EXIT_FAILURE) {
     return result;
   }
 
+  char **users = (char**) calloc(1, sizeof(char*));
+  if (users == NULL) {
+    printf("There was an error allocating the memory.\n");
+    return EXIT_FAILURE;
+  }
+  int usersSize = 0;
+  result = parseUsers(argc, argv, users, &usersSize);
+  if (result == EXIT_FAILURE) {
+    return result;
+  }
+
+
   // release the allocated memory
   free(options);
+  for (int i = 0; i < usersSize; i++) {
+    free(users[i]);
+  }
+  free(users);
 
   return result;
 }
 
-int parseArguments(int argc, char* argv[], options_t *options) {
+int parseOptions(int argc, char* argv[], options_t *options) {
   // check the provided arguments
   // skip the first argument that is the program name
   for (int i = 1; i < argc; i++) {
-    printf("argc = %d\n", i);
-    printf("argv[%d] --> %s\n", i, argv[i]);
 
+    // check if the argument is an option
     if (argv[i][0] == '-') {
+      // a string is an array of characters
+      // in which the \0 is the termination character
       for (int j = 1; argv[i][j] != '\0'; j++) {
         bool found = false;
         if (argv[i][j] == 'l') {
@@ -60,6 +77,39 @@ int parseArguments(int argc, char* argv[], options_t *options) {
     }
   }
 
+  return EXIT_SUCCESS;
+}
+
+int parseUsers(int argc, char* argv[], char **users, int *usersSize) {
+  // check the provided arguments
+  // skip the first argument that is the program name
+  int count = 0;
+  for (int i = 1; i < argc; i++) {
+
+    // check if the argument is a user
+    if (argv[i][0] != '-') {
+
+      if (count > 0) {
+        // re-allocate memory for new users
+        users = (char**) realloc(users, (count + 1) * sizeof(char*));
+        if (users == NULL) {
+          printf("There was an error allocating the memory.\n");
+          return EXIT_FAILURE;
+        }
+      }
+      // allocate memory for user information
+      users[count] = (char*) calloc(USER_LENGTH, sizeof(char));
+      if (users[count] == NULL) {
+        printf("There was an error allocating the memory.\n");
+        return EXIT_FAILURE;
+      }
+
+      strncpy(users[count], argv[i], USER_LENGTH);
+      count++;
+    }
+  }
+
+  *usersSize = count;
   return EXIT_SUCCESS;
 }
 
--- homework1/finger/finger.h
+++ homework1/finger/finger.h
@@ -1,5 +1,7 @@
 #include <stdbool.h> // included to use the bool data type
 
+#define USER_LENGTH 100
+
 typedef struct {
   bool l;
   bool m;
@@ -7,5 +9,6 @@ typedef struct {
   bool p;
 } options_t;
 
-int parseArguments(int argc, char* argv[], options_t *options);
+int parseOptions(int argc, char* argv[], options_t *options);
+int parseUsers(int argc, char* argv[], char **users, int *usersSize);
 void printHelp();
