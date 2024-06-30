1e3d2dd16dff46f422406de2e84cbfc319f2db4e
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2024-06-30 11:14:01 +0200

    rename file description variable in utils

--- homework2/utils/utils.c
+++ homework2/utils/utils.c
@@ -3,25 +3,25 @@
 #include "utils.h"
 
 int* createSocket() {
-  int listenFd = socket(AF_INET, SOCK_STREAM, 0); // AF_INET is the TCP/IP protocol
+  int fd = socket(AF_INET, SOCK_STREAM, 0); // AF_INET is the TCP/IP protocol
                                                   // SOCK_STREAM is the TCP transfer service
-  if (listenFd == -1) {
+  if (fd == -1) {
     perror("Error while opening the socket");
     return NULL;
   }
 
   printf("Socket has been created ...\n");
-  int* listenFdPtr = &listenFd;
-  return listenFdPtr;
+  int* fdPtr = &fd;
+  return fdPtr;
 }
 
-struct sockaddr_in* buildAddress(int listenFd, char* address, int port) {
+struct sockaddr_in* buildAddress(int fd, char* address, int port) {
   struct sockaddr_in* serverAddress;
   memset(serverAddress, 0, sizeof(serverAddress)); // be sure that the area is cleaned
   serverAddress->sin_family = AF_INET;
   serverAddress->sin_addr.s_addr = inet_addr(address);
   serverAddress->sin_port = htons(port);
-  if (bind(listenFd, (struct sockaddr*)serverAddress, sizeof(serverAddress))) {
+  if (bind(fd, (struct sockaddr*)serverAddress, sizeof(serverAddress))) {
     perror("Error while setting the address to the socket");
     return NULL;
   }
